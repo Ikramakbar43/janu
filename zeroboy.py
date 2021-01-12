b8fb459d240057b98df74f327821a31ba3a1c6ea
Ikramakbar43 <77304599+Ikramakbar43@users.noreply.github.com>
2021-01-12 11:07:17 +0500

    ikramakbar63.py

--- zeroboy.py
+++ zeroboy.py
@@ -47,19 +47,13 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+logo = """ I̴̪̰̭̞͍͌̐͌ķ̶̧̆̈́ṙ̵̡̝͎̪̼̬̘͉̔̃̾̐̓̈́͠á̵̜̬̘̯̻̪͜ḿ̵̤̱̦̘̜̟̻͇ͅ ̷̣̦̘̙̼̖̑͘̕A̷̟̳͇̩̘͘͝k̵̖̘̽͆̀͝͝b̸̧̳̙̤̫͎̥̣̬̋͊̏a̷̢̻̥̦͒̌̕̕͠͝ͅŗ̵̛̞̼͓̺̹̹̹͔̂͊̋̈́̐̀́
+
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mwhatsApp +923082839268
+\033[1;96mInstagram \033[1;93m: \033[1;Flow Ikram_Akbar64
+\033[1;96mFacebook  \033[1;93m: \033[1; ikramakbar64
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/ikramakbar63/janu
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +74,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mwhatspp +923082839268
+\033[1;96mInstagram \033[1;93m: \033[1;92mikram_Akbar64
+\033[1;96mFacebook  \033[1;93m: \033[1;92m ikramakbar64
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/ikramakbar63/janu
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "janu"
+CorrectPassword = "janu"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +94,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.Youtube.com/UC2X4F2qRcrYBJH9aDA5cOcQ')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.Youtube.com/UC2X4F2qRcrYBJH9aDA5cOcQ')
 
 def login():
 	os.system('clear')
